a8680687545a07a255609788f92cd8ed586ad3a3
Jesse Whiting <little.dude.jesse@gmail.com>
2021-04-07 10:30:49 -0600

    Added fps stuff
    Can now Track and set a fixedfps

--- Assets/Scripts/Engine/World.cs
+++ Assets/Scripts/Engine/World.cs
@@ -7,6 +7,13 @@ public class World : MonoBehaviour
 
     public BoolData simulate;
     public FloatData gravity;
+    public FloatData fixedFPS;
+    public StringData FPSData;
+    public FloatData FPSRawValue;
+
+    private float timeAccumulator;
+    public float fixedDeltaTime { get { return 1.0f / fixedFPS.value; } }
+
 
     static World instance;
     public static World Instance { get
@@ -29,11 +36,22 @@ public class World : MonoBehaviour
         if (!simulate.value) return;
 
         float dt = Time.deltaTime;
+        timeAccumulator += dt;
 
-        bodies.ForEach(body => body.Step(dt));
-        bodies.ForEach(body => Integrator.ExplicitEuler(body, dt));
+        while (timeAccumulator > fixedDeltaTime)
+        {
+            float smoothing = .95f;
+            float rawFPS = (1.0f / timeAccumulator);
+            FPSRawValue.value = (rawFPS * smoothing) + (FPSRawValue.value * (1.0f - smoothing));
+            FPSData.value = FPSRawValue.value + "";
 
-        bodies.ForEach(body => body.force = Vector2.zero);
-        bodies.ForEach(body => body.acceleration = Vector2.zero);
+            bodies.ForEach(body => body.Step(fixedDeltaTime));
+            bodies.ForEach(body => Integrator.ExplicitEuler(body, fixedDeltaTime));
+
+            bodies.ForEach(body => body.force = Vector2.zero);
+            bodies.ForEach(body => body.acceleration = Vector2.zero);
+
+            timeAccumulator -= fixedDeltaTime;
+        }
     }
 }
